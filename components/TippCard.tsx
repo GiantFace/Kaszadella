964fe997e68b73611550daa38c80750d8011be4e
GiantFace <modroczkyferenc0221@gmail.com>
2025-04-28 11:19:54 +0200

    Cards

--- components/TippCard.tsx
+++ components/TippCard.tsx
@@ -20,12 +20,12 @@ const TippCard: React.FC<TippCardProps> = ({ tip }) => {
     <div className="flipbox-item" style={{ perspective: "1000px" }}>
       {/* Kártya belső tartalma */}
       <div
-        className="flipbox-inner mt-8 md:mt-0 absolute"
+        className="flipbox-inner mt-5 md:mt-0 absolute"
         style={{ transformStyle: "preserve-3d" }}
       >
         {/* Elülső oldal */}
         <div
-          className="flipbox-front bg-gray-800 min-h-[50px] md:min-h-[200px] p-4 flex flex-col items-center"
+          className="flipbox-front bg-gray-800 min-h-[80px] md:min-h-[200px] p-4 flex flex-col items-center"
           style={{ backfaceVisibility: "hidden" }}
         >
           <h3 className="font-bold text-2xl text-white mb-5 text-center">
@@ -38,12 +38,12 @@ const TippCard: React.FC<TippCardProps> = ({ tip }) => {
         <div className="flipbox-back bg-gray-800 p-4 flex flex-col items-center min-h-[250px] md:min-h-[400px] gap-4">
           {/* Felül: cím */}
           {tip.accuracy !== undefined && (
-            <div className="justify-center top-[-29] transform  bg-gradient-to-r from-yellow-400 to-yellow-500 text-black font-bold text-sm px-6 py-2 rounded-full shadow-lg z-[-20] trapezoid-shape">
+            <div className="justify-center top-[-40] transform  bg-gradient-to-r from-yellow-400 to-yellow-500 text-black font-bold text-sm px-4 py-2 rounded-full shadow-lg z-[-20] trapezoid-shape">
               🎯 Találati arány: {tip.accuracy}%
             </div>
           )}
 
-          <h3 className="font-bold text-2xl text-white text-center max-h-1.5">
+          <h3 className="font-bold text-2xl text-white text-center max-h-1">
             {tip.title}
           </h3>
           {/* Kép */}
@@ -66,7 +66,7 @@ const TippCard: React.FC<TippCardProps> = ({ tip }) => {
             {tip.back_description}
           </p>
           {/* Gomb */}
-          <button className="flixbox-button text-white rounded-lg font-semibold w-full  h-10 hover:bg-primary-turquoise hover:text-black transition-transform hover:scale-105 min-h-9">
+          <button className="flixbox-button bg-yellow-500 mt-[-20] text-black rounded-lg font-semibold w-full absolu h-10 hover:bg-primary-turquoise hover:text-black transition-transform hover:scale-105 min-h-9">
             <Link href="/subscription">Aktiválom</Link>
           </button>
         </div>
